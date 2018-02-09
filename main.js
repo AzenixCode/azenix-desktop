5164b1085b142d1bbb436b0d0d1da7f4b17ad78f
dnovacik <novacik.daniel@gmail.com>
2018-02-09 08:40:14 +0100

    closes #3, closes #7
    - implementation of eslint
    - fix to vulnerability issue in older electron

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const url = require('url');
 const openAboutWindow = require('about-window').default;
 const windowStateKeeper = require('electron-window-state');
 
-const app = electron.app;
+const App = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const Menu = electron.Menu;
 
@@ -126,18 +126,18 @@ Menu.setApplicationMenu(menu)
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+App.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
+App.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit();
+    App.quit();
   }
 });
 
-app.on('activate', () => {
+App.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
